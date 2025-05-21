3be8cfbb6133b715df09cad04b6ac1a62d5243f1
issatengen <issatengen@outlook.com>
2025-05-21 13:14:25 +0100

    Auto-commit: Wed 05/21/2025 13:14:25.58

--- auto_git_push.bat
+++ auto_git_push.bat
@@ -1,5 +1,13 @@
 :: auto_git_push.bat
-cd /d "C:\Users\nji\Desktop\trustfulGlobal"
+cd /d "C:\Users\nji\Desktop\trustfulGlobal\trustfulGlobal"
 git add .
 git commit -m "Auto-commit: %date% %time%"
-git push
+git pull
+:: Resolve merge conflicts if any
+git merge --continue
+:: git rebase --abort
+:: git rebase --continue
+:: git pull --rebase
+:: git pull --rebase origin main
+git branch -M main
+git push -u origin main
